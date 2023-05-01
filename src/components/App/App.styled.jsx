d7871c98d61ebc908d49c71d78ae7cfb7fffce2f
PilyaP <ppaexpert@gmail.com>
2023-05-01 18:16:34 +0300

    2

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-    margin: 30px auto;
+margin: 30px auto;
   display: flex;
   flex-direction: column;
   align-items: center;
